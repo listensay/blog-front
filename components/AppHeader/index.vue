7cfba9807709c7a9ce9d5fb459aa1c0414ea383c
codemmk <2107228359@qq.com>
2023-10-05 20:15:20 +0800

    占位

--- components/AppHeader/index.vue
+++ components/AppHeader/index.vue
@@ -6,17 +6,22 @@ const route = useRoute()
 <template>
   <div class="app-header">
     <div class="navbar px-8">
-      <div class="text-black">
-        <template v-if="route.fullPath === '/'">
-          <span>听的说个人空间</span>
-        </template>
-        <template v-else>
-          <NuxtLink to="/">
-            <div class="flex flex-row items-center">
-              <el-icon class="mr-1"><ArrowLeft /></el-icon> <span>返回</span>
-            </div>
-          </NuxtLink>
-        </template>
+      <div class="text-black flex items-center justify-between">
+        <div>
+          <template v-if="route.fullPath === '/'">
+            <span>听的说个人空间</span>
+          </template>
+          <template v-else>
+            <NuxtLink to="/">
+              <div class="flex flex-row items-center">
+                <el-icon class="mr-1"><ArrowLeft /></el-icon> <span>返回</span>
+              </div>
+            </NuxtLink>
+          </template>
+        </div>
+        <div>
+          <!-- <el-button link><el-icon size="20"><Menu /></el-icon></el-button> -->
+        </div>
       </div>
     </div>
     <template v-if="route.fullPath === '/'">
